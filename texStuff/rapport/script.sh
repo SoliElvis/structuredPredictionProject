e44aa710b4466883b657573b42200dc559717466
frederic_boileau <frederic.boileau@protonmail.com>
2019-04-22 16:05:03 -0400

    "coller ensemble les boutes"

--- texStuff/rapport/script.sh
+++ texStuff/rapport/script.sh
@@ -4,3 +4,8 @@ sed -n '/\\section{.*}/,/\\end{document}/p' extra_grad.tex > temp.tex;
 sed '/\\end{document}.*/d' temp.tex > extraGradMain.tex
 sed -i 's/section/subsection/g' extraGradMain.tex
 rm temp.tex
+
+sed -n '/\\section{.*}/,/\\end{document}/p' rapport_edited.tex > temp.tex;
+sed '/\\end{document}.*/d' temp.tex > rapport_edited_main.tex
+sed -i 's/section/subsection/g' rapport_edited_main.tex
+rm temp.tex
