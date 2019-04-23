529a53ca2a8f69694676838086bc89a13ad1bc16
frederic_boileau <frederic.boileau@protonmail.com>
2019-04-23 16:57:00 -0400

    batch download petites corrections et gemini

--- expcode/dataprep/data_prep.py
+++ expcode/dataprep/data_prep.py
@@ -16,112 +16,118 @@ import time
 
 from typing import List
 
-save_dir = "FEC_dataset"
+save_dir = "./FEC_dataset"
 train_csv = "faceexp-comparison-data-train-public.csv"
 test_csv = "faceexp-comparison-data-test-public.csv"
 tt= ("train", "test")
 
 class ImageDataPrep:
-	def __init__(self,save_dir):
-		self.save_dir = save_dir
-		self.imagesDir = os.path.join(save_dir, "images")
-		self.dataDir = os.path.join(save_dir, "data")
+  def __init__(self,save_dir):
+    self.save_dir = save_dir
+    self.imagesDir = os.path.join(save_dir, "images")
+    self.dataDir = os.path.join(save_dir, "data")
 
-		self.dataSets = [os.path.join(self.save_dir, f) for f in [train_csv,test_csv]]
-		self.dataPathDict = {"train" : self.dataSets[0],
-													"test"  : self.dataSets[1]}
+    self.dataSets = [os.path.join(self.save_dir, f) for f in [train_csv,test_csv]]
+    self.dataPathDict = {"train" : self.dataSets[0],
+                          "test"  : self.dataSets[1]}
 
 
-	def prep_file_system(self):
-		dirsToCreate = tt_join_paths([self.imagesDir,self.dataDir])
-		createFolder(dirsToCreate)
-		return dirsToCreate
+  def prep_file_system(self):
+    dirsToCreate = tt_join_paths([self.imagesDir,self.dataDir])
+    createFolder(dirsToCreate)
+    return dirsToCreate
 
 
 class ImageDataPrepFEC(ImageDataPrep):
+	def __init__(self,save_dir):
+		ImageDataPrep.__init__(self,save_dir)
+
 	def batch_download_images(self):
 		urlSlots = [0,5,10]
 		ssl._create_default_https_context = ssl._create_unverified_context
-		for testOrTrain, path in dataPathDict:
+		for testOrTrain, path in self.dataPathDict.items():
 			with open(path) as csv_file:
 				csv_reader = csv.reader(csv_file, delimiter=',')
 				for id, row in enumerate(csv_reader):
 					for slot in urlSlots:
 						response = requests.get(row[slot])
-						if (reponse.status_code == 200): break
+						if (response.status_code == 200): break
 						if testOrTrain == "train":
-							with open(os.path.join(dataPathDict["train"], str(id) + "-" + str(slot)), 'wb') as im:
-								im.write(reponse.content)
+							path = os.path.join(self.imagesDir,"train", str(id) + "-" + str(slot))
+							print(path)
+							break
+							with open(os.path.join(path ,'wb+'))as im:
+								im.write(response.content)
 						else :
-							with open(os.path.join(dataPathDict["test"], str(id) + "-" + str(slot)), 'wb') as im:
-								im.write(reponse.content)
+							with open(os.path.join(self.imagesDir,"test", str(id) + "-" + str(slot)), 'wb+') as im:
+								im.write(response.content)
 
 
 
- def download_data():
-   # download the training set and test set
-	ssl._create_default_https_context = ssl._create_unverified_context
+ # def download_data():
+ #   # download the training set and test set
+ #  ssl._create_default_https_context = ssl._create_unverified_context
 
-	with open(args.data_path) as csv_file:
-		csv_reader = csv.reader(csv_file, delimiter=',')
+ #  with open(args.data_path) as csv_file:
+ #    csv_reader = csv.reader(csv_file, delimiter=',')
 
-		for dataset in ["train", "test"]:
-			with open(os.path.join(args.save_dir, dataset + ".npy"), 'ab') as f:
-				to_save = np.empty([0, (3 * args.dim) ** 2 + 7])
-				for id, row in enumerate(csv_reader):
-					im_tup = []
-					for i in [0, 5, 10]:
-					# load the image
-						response = requests.get(row[i])
-						try:
-							im = Image.open(BytesIO(response.content))
-						except OSError:
-							break
+ #    for dataset in ["train", "test"]:
+ #      with open(os.path.join(args.save_dir, dataset + ".npy"), 'ab') as f:
+ #        to_save = np.empty([0, (3 * args.dim) ** 2 + 7])
+ #        for id, row in enumerate(csv_reader):
+ #          im_tup = []
+ #          for i in [0, 5, 10]:
+ #          # load the image
+ #            response = requests.get(row[i])
+ #            try:
+ #              im = Image.open(BytesIO(response.content))
+ #            except OSError:
+ #              break
 
-					# crop it to only keep face info
-					w, h = im.size
-					left, up = np.rint(float(row[i + 1]) * w), np.rint(float(row[i + 3]) * h)
-					right, down = np.rint(float(row[i + 2]) * w), np.rint(float(row[i + 4]) * h)
-					area = (int(left), int(up), int(right), int(down))
-					crop = im.crop(area)
+ #          # crop it to only keep face info
+ #          w, h = im.size
+ #          left, up = np.rint(float(row[i + 1]) * w), np.rint(float(row[i + 3]) * h)
+ #          right, down = np.rint(float(row[i + 2]) * w), np.rint(float(row[i + 4]) * h)
+ #          area = (int(left), int(up), int(right), int(down))
+ #          crop = im.crop(area)
 
-					# resize the image to the right proportion
-					Crop = np.asarray(ImageOps.fit(crop,
-																						(args.dim, args.dim), Image.ANTIALIAS), dtype=np.float32)
+ #          # resize the image to the right proportion
+ #          Crop = np.asarray(ImageOps.fit(crop,
+ #                                            (args.dim, args.dim), Image.ANTIALIAS), dtype=np.float32)
 
-					# append the image to image tuple
-					im_tup.append(crop)
+ #          # append the image to image tuple
+ #          im_tup.append(crop)
 
-					# if the image tuple does  not contain three images skip
-					if not len(im_tup) == 3:
-							continue
+ #          # if the image tuple does  not contain three images skip
+ #          if not len(im_tup) == 3:
+ #              continue
 
-					# extract features and label
-					try:
-						features = np.stack(im_tup).ravel()
-					except ValueError:
-						continue
+ #          # extract features and label
+ #          try:
+ #            features = np.stack(im_tup).ravel()
+ #          except ValueError:
+ #            continue
 
-					label = np.asarray([row[17 + i * 2] for i in range(6)], dtype=np.float32)
-					if row[15] == "ONE_CLASS_TRIPLET":
-						label = np.concatenate(([1.], label)).astype("float32")
-					elif row[15] == "TWO_CLASS_TRIPLET":
-						label = np.concatenate(([2.], label)).astype("float32")
-					else:
-						label = np.concatenate(([3], label)).astype("float32")
+ #          label = np.asarray([row[17 + i * 2] for i in range(6)], dtype=np.float32)
+ #          if row[15] == "ONE_CLASS_TRIPLET":
+ #            label = np.concatenate(([1.], label)).astype("float32")
+ #          elif row[15] == "TWO_CLASS_TRIPLET":
+ #            label = np.concatenate(([2.], label)).astype("float32")
+ #          else:
+ #            label = np.concatenate(([3], label)).astype("float32")
 
-					im_tup = np.concatenate((features, label)).reshape([1, -1])
+ #          im_tup = np.concatenate((features, label)).reshape([1, -1])
 
-					# add the row to_save
-					to_save = np.concatenate((to_save, im_tup))
+ #          # add the row to_save
+ #          to_save = np.concatenate((to_save, im_tup))
 
-					if id > 10000 and dataset == "train":
-							break
-					elif id > 3000 and dataset == "test":
-							break
-						np.save(f.name, to_save)
-						f.close()
-						csv_file.close()
+ #          if id > 10000 and dataset == "train":
+ #              break
+ #          elif id > 3000 and dataset == "test":
+ #              break
+ #            np.save(f.name, to_save)
+ #            f.close()
+ #            csv_file.close()
 
 
 def main():
