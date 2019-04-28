ea9fa61d00bd3994360a70020974ab15cbb19c5e
frederic_boileau <frederic.boileau@protonmail.com>
2019-04-28 16:12:04 -0400

    bib and all

--- texStuff/rapport/auto/mainProject.el
+++ texStuff/rapport/auto/mainProject.el
@@ -1,10 +1,17 @@
 (TeX-add-style-hook
  "mainProject"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("biblatex" "backend=biber" "style=numeric" "citestyle=numeric")))
    (TeX-run-style-hooks
     "premath"
     "extraGradMain"
     "rapport_edited_main"
-    "exp"))
+    "exp"
+    "discussion"
+    "conclusion"
+    "biblatex")
+   (LaTeX-add-bibliographies
+    "tocite"))
  :latex)
 
