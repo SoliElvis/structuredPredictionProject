73368a65a4c97bd71c826c33b3af44b6264aa937
frederic_boileau <frederic.boileau@protonmail.com>
2019-04-28 21:58:04 -0400

    "intro done et extragrad into"

--- texStuff/rapport/auto/mainProject.el
+++ texStuff/rapport/auto/mainProject.el
@@ -2,7 +2,15 @@
  "mainProject"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("biblatex" "backend=biber" "style=numeric" "citestyle=numeric") ("eucal" "mathcal")))
+                     '(("biblatex" "backend=biber" "citestyle=authoryear-ibid" "natbib=true")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "premath"
     "intro"
@@ -16,8 +24,9 @@
     "conclusion"
     "appendix"
     "biblatex"
-    "eucal")
+    "csquotes")
    (LaTeX-add-bibliographies
+    "tocite.bib"
     "tocite"))
  :latex)
 
