e49129a3990ef36eb7f2e79308f41698040f198b
frederic_boileau <frederic.boileau@protonmail.com>
2019-04-30 17:10:42 -0400

    "je sais pu"

--- texStuff/rapport/auto/premath.el
+++ texStuff/rapport/auto/premath.el
@@ -2,13 +2,13 @@
  "premath"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("isodate" "english") ("geometry" "margin=1in") ("inputenc" "utf8") ("fontenc" "T1") ("hyperref" "bookmarks=true")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+                     '(("algorithm" "plain") ("isodate" "english") ("geometry" "margin=1in") ("inputenc" "utf8") ("fontenc" "T1") ("hyperref" "bookmarks=true")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
