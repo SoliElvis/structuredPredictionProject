aa2d6f3460540cd93910d1585be869999d6b704c
frederic_boileau <frederic.boileau@protonmail.com>
2019-04-30 22:25:35 -0400

    "sais pu"

--- texStuff/rapport/auto/mainProject.el
+++ texStuff/rapport/auto/mainProject.el
@@ -3,12 +3,12 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("biblatex" "backend=biber" "citestyle=authoryear-ibid" "natbib=true") ("appendix" "toc" "page")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
@@ -19,6 +19,8 @@
     "classical_fw"
     "bcfwMain"
     "exp"
+    "discussion"
+    "conclusion"
     "cvxanalysis"
     "app_extra"
     "app_fw"
